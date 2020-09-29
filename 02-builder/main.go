e287671d5bdb2000e374a024bffd3b42895f4b2e
Richa <programmer.richa@gmail.com>
2020-09-29 12:52:19 +0530

    Functional builder implementation

--- 02-builder/main.go
+++ 02-builder/main.go
@@ -10,4 +10,10 @@ func main() {
 		SetBranch("Head Office").
 		SetSalary(10000)
 	fmt.Println(emp)
+
+	// Using Functional Builder
+	pb := PersonBuilder{}
+	r := pb.Name("Richa").Position("Software Developer").Build()
+	fmt.Println(r)
+
 }
